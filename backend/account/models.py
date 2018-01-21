40e6d4894faeffa0aaf88c09caaaef8fc7aaf566
Elie Lagmay <elielagmay@gmail.com>
2018-01-21 20:43:59 +0800

    Add Category and Account models

---
+++ backend/account/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+
+
+class Category(models.Model):
+    ledger = models.ForeignKey(
+        'ledger.Ledger',
+        on_delete=models.PROTECT,
+        related_name='categories'
+    )
+    name = models.CharField(max_length=255)
+    sort = models.IntegerField(default=0)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name_plural = 'categories'
+        unique_together = ('ledger', 'name')
+        ordering = ('sort', 'id')
+
+    def __str__(self):
+        return self.name
+
+
+class Account(models.Model):
+    category = models.ForeignKey(
+        'account.Category',
+        on_delete=models.PROTECT,
+        related_name='accounts'
+    )
+    name = models.CharField(max_length=255)
+    sort = models.IntegerField(default=0)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        unique_together = ('category', 'name')
+        ordering = ('sort', 'id')
+
+    def __str__(self):
+        return self.name
