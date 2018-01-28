3f1c0f9be5360eb354e1029aed9d603f9046d9b5
Elie Lagmay <elielagmay@gmail.com>
2018-01-28 21:33:27 +0800

    Add NotFoundPage component

---
+++ frontend/src/notFound/NotFoundPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class NotFoundPage extends React.Component {
+  render () {
+    return (
+      <div className='NotFoundPage'>Page Not Found</div>
+    )
+  }
+}
+
+export default NotFoundPage
+export { NotFoundPage }
