94dd957f39630a3571ac6fad79b534af0536a479
Elie Lagmay <elielagmay@gmail.com>
2018-02-03 11:46:40 +0800

    Convert SidebarGroup to functional component

--- frontend/src/sidebar/SidebarGroup.js
+++ frontend/src/sidebar/SidebarGroup.js
@@ -2,18 +2,12 @@ import React from 'react'
 import injectSheet from 'react-jss'
 import { styles } from './styles'
 
-class SidebarGroup extends React.Component {
-  render () {
-    const { classes, title, children } = this.props
-
-    return (
-      <div className={classes.group}>
-        {!title ? null : <div className={classes.groupTitle}>{title}</div>}
-        {children}
-      </div>
-    )
-  }
-}
+const SidebarGroup = ({ classes, title, children }) => (
+  <div className={classes.group}>
+    {!title ? null : <div className={classes.groupTitle}>{title}</div>}
+    {children}
+  </div>
+)
 
 export default injectSheet(styles)(SidebarGroup)
 export { SidebarGroup }
