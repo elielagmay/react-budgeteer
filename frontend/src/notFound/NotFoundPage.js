41fcaf28ae92296baa2364af36a9326bd1e339b3
Elie Lagmay <elielagmay@gmail.com>
2018-02-03 11:46:21 +0800

    Convert NotFoundPage to functional component

--- frontend/src/notFound/NotFoundPage.js
+++ frontend/src/notFound/NotFoundPage.js
@@ -1,12 +1,8 @@
 import React from 'react'
 
-class NotFoundPage extends React.Component {
-  render () {
-    return (
-      <div className='NotFoundPage'>Page Not Found</div>
-    )
-  }
-}
+const NotFoundPage = () =>  (
+  <div className='NotFoundPage'>Page Not Found</div>
+)
 
 export default NotFoundPage
 export { NotFoundPage }
