5346654b6fa1324cd1dbed8f17cda759a8857896
Elie Lagmay <elielagmay@gmail.com>
2018-02-05 11:31:29 +0800

    Add Budget page

---
+++ frontend/src/budget/BudgetPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class BudgetPage extends React.Component {
+  render () {
+    return (
+      <div className='BudgetPage'>Budget Page</div>
+    )
+  }
+}
+
+export default BudgetPage
+export { BudgetPage }
