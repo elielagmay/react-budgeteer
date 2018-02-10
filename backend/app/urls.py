416391e3506d72a6d1c5fb2a3bda2b09e1085e8a
Elie Lagmay <elielagmay@gmail.com>
2018-02-10 13:09:09 +0800

    Use re_path for serving frontend app

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,14 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('graphql/', views.graphql, name='graphql'),
     path('health-check/', views.health_check, name='health-check'),
-    path('', views.index, name='index'),
-    path('budgets', views.index),
-    path('transactions', views.index),
-    path('reports', views.index),
-    path('account/<int:id>', views.index),
+    re_path('', views.index, name='index'),
 ]
