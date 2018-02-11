ea066db1aa36ee8cf1c7f5da18b873d1a77be043
Elie Lagmay <elielagmay@gmail.com>
2018-02-11 10:19:26 +0800

    Include ledger id in routes

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -10,41 +10,46 @@ import NotFoundPage from '../notFound/NotFoundPage'
 import { userQuery } from './queries'
 import { styles } from './styles'
 
-class App extends React.Component {
-  render () {
-    const { classes, data } = this.props
-
-    if (data.loading) {
-      return null // todo - add loading indicator
-    }
-
-    return (
-      <BrowserRouter>
-        <div className={classes.root}>
-          <div className={classes.head}>
-            <Navbar user={data.user} />
-          </div>
-          <div className={classes.side}>
-            <Sidebar />
-          </div>
-          <div className={classes.main}>
-            <Switch>
-              <Redirect exact from='/' to='/budgets' />
-              <Route path='/budgets' component={BudgetPage} />
-              <Route path='/transactions' component={TransactionPage} />
-              <Route path='/reports' component={NotFoundPage} />
-              <Route path='/account/:id' component={NotFoundPage} />
-              <Route component={NotFoundPage} />
-            </Switch>
-          </div>
-        </div>
-      </BrowserRouter>
-    )
+export const App = ({ classes, data }) => {
+  if (data.loading) {
+    return null // todo - add loading indicator
   }
+
+  const user = data.user
+  const ledger = user.ledgers.edges.length > 0
+    ? user.ledgers.edges[0].node
+    : null
+
+  if (!user || !ledger) {
+    return null
+  }
+
+  return (
+    <BrowserRouter>
+      <div className={classes.root}>
+        <div className={classes.head}>
+          <Navbar user={data.user} />
+        </div>
+        <div className={classes.side}>
+          <Sidebar ledger={ledger} />
+        </div>
+        <div className={classes.main}>
+          <Switch>
+            <Route path='/settings' component={NotFoundPage} />
+            <Route path='/ledgers' component={NotFoundPage} />
+            <Route path='/ledger/:id/budgets' component={BudgetPage} />
+            <Route path='/ledger/:id/transactions' component={TransactionPage} />
+            <Route path='/ledger/:id/accounts' component={NotFoundPage} />
+            <Route path='/ledger/:id/reports' component={NotFoundPage} />
+            <Route component={NotFoundPage} />
+          </Switch>
+        </div>
+      </div>
+    </BrowserRouter>
+  )
 }
 
 export default compose(
   graphql(userQuery),
   injectSheet(styles)
 )(App)
-export { App }
